f017a111a58b08e0d36ec28bd1c60e7a8537cfab
fnrir <fnr1r0@protonmail.com>
2025-12-26 21:30:21 +0100

    feat: add a couple non-import lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
 #![doc = include_str!("../README.md")]
+#![deny(meta_variable_misuse)]
 #![deny(unsafe_op_in_unsafe_fn)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(unreachable_pub)]
 #![warn(clippy::alloc_instead_of_core)]
 #![warn(clippy::std_instead_of_alloc)]
 #![warn(clippy::std_instead_of_core)]
+#![warn(clippy::missing_const_for_fn)]
 #![no_std]
 
+#[allow(
+    missing_docs,
+    clippy::missing_const_for_fn,
+    reason = "This is boilerplate"
+)]
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
