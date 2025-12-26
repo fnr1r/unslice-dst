53a2baeda157b8f27cf7a726c26e856fca6c4e30
fnrir <fnr1r0@protonmail.com>
2025-12-26 23:27:26 +0100

    fix: remove boilerplate
    This will allow me to drop the MSRV lower.

--- src/lib.rs
+++ src/lib.rs
@@ -20,23 +20,3 @@ pub mod cast;
 ///
 /// It's assumed to always be of size 0.
 pub type AnyDst = [()];
-
-#[allow(
-    missing_docs,
-    clippy::missing_const_for_fn,
-    reason = "This is boilerplate"
-)]
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
