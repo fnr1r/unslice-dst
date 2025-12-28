dffadc3e69607e049058f791b71e8347a155c8fb
fnrir <fnr1r0@protonmail.com>
2025-12-28 14:06:41 +0100

    feat(cast/fat_ptr): add complex dst test

--- src/fat_ptr/tests.rs
+++ src/fat_ptr/tests.rs
@@ -1,4 +1,5 @@
 use super::{dst_len, SliceDstPointer};
+use crate::cast::{dst_cast_const, dst_cast_impl};
 
 const LEN: usize = 420;
 const DATA: &[u8] = &[12; LEN];
@@ -47,4 +48,36 @@ mod cstr {
         // NOTE: Count bytes can't be used, since we need to include the null
         assert_eq!(dst_len(TEST_CSTR), TEST_CSTR.to_bytes_with_nul().len());
     }
+}
+
+const EMPTY_ALIGN_FIX: [i32; 0] = [];
+#[rustfmt::skip]
+const TEST_DATA_M: [u8; 12] = [
+    // n: i32 LE
+    0x80, 0x00, 0x00, 0x00,
+    // text: str
+    b'h', b'e', b'l', b'l', b'o',
+    // _padding
+    0x00, 0x00, 0x00,
+];
+const TEST_DATA: &([i32; 0], [u8; 12]) = &(EMPTY_ALIGN_FIX, TEST_DATA_M);
+
+#[derive(Debug)]
+struct TestStruct {
+    n: [u8; 4],
+    text: str,
+}
+
+dst_cast_impl!(for TestStruct);
+
+#[test]
+fn assert_complex_const_dst_works() {
+    let slice = TEST_DATA.1.as_ref();
+    let data = dst_cast_const::<_, TestStruct>(slice);
+    let mut data = SliceDstPointer::from_ptr(data);
+    data.metadata = 5;
+    let ptr = data.into_ptr::<TestStruct>();
+    let data = unsafe { ptr.as_ref().unwrap_unchecked() };
+    assert_eq!(i32::from_be_bytes(data.n), i32::MIN);
+    assert_eq!(&data.text, "hello");
 }
