b94d13ffc6abc56d8ce369b3c6e4b5be7dc7faf0
fnrir <fnr1r0@protonmail.com>
2025-12-31 20:17:47 +0100

    feat(cast): add new tests

--- src/cast/tests.rs
+++ src/cast/tests.rs
@@ -1,13 +1,31 @@
-use super::DstCast;
+use super::{dst_cast_const, DstCast};
+
+const TEST_DATA: &[u8] = &[130; 32];
+const TEST_RESULT: isize = -126;
+
+fn inner(f: impl Fn(&[u8]) -> &[i8]) {
+    let data_retyped = f(TEST_DATA);
+    for i in data_retyped.iter().copied() {
+        assert_eq!(i as isize, TEST_RESULT);
+    }
+}
 
 #[test]
 fn assert_cast_works() {
-    let a = [69i8; 32];
-    let b = <[u8]>::cast_dst_const(a.as_ref());
-    // SAFETY: the pointer is created from a valid reference to `a`
-    //   the types are compatible in terms of size and as-cast-able
-    let b = unsafe { b.as_ref().unwrap_unchecked() };
-    for (c, d) in a.iter().zip(b) {
-        assert_eq!(Ok(*c), i8::try_from(*d));
-    }
+    inner(|value| {
+        let b = <[i8]>::cast_dst_const(value);
+        // SAFETY: the pointer is created from a valid reference to `a`
+        //   the types are compatible in terms of size and as-cast-able
+        unsafe { b.as_ref().unwrap_unchecked() }
+    });
+}
+
+#[test]
+fn assert_const_cast_works() {
+    inner(|value| {
+        let b = dst_cast_const::<[u8], [i8]>(value);
+        // SAFETY: the pointer is created from a valid reference to `a`
+        //   the types are compatible in terms of size and as-cast-able
+        unsafe { b.as_ref().unwrap_unchecked() }
+    });
 }
