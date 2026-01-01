a95935cbfe8a77386a6b161a06a60105bbf119a2
fnrir <fnr1r0@protonmail.com>
2026-01-01 13:17:59 +0100

    feat(container): add NonNull alloc utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,5 @@
 pub(crate) use self::transmute_lax::transmute_lax;
 
+pub(crate) mod nalloc;
 pub(crate) mod slice;
 mod transmute_lax;
---
+++ src/utils/nalloc.rs
@@ -0,0 +1,42 @@
+use alloc::alloc::{alloc, dealloc, handle_alloc_error};
+use core::{alloc::Layout, ptr::NonNull};
+
+#[cfg_attr(not(feature = "container_unseal"), allow(dead_code))]
+#[inline]
+unsafe fn nonnull_alloc_raw(layout: Layout) -> Option<NonNull<u8>> {
+    NonNull::new(unsafe { alloc(layout) })
+}
+
+#[cfg_attr(not(feature = "container_unseal"), allow(dead_code))]
+#[inline]
+unsafe fn nonnull_alloc(layout: Layout) -> NonNull<u8> {
+    unsafe { nonnull_alloc_raw(layout) }.unwrap_or_else(|| handle_alloc_error(layout))
+}
+
+/// # Safety
+///
+/// Although the UB is handled, this function is unsafe to match the others.
+#[cfg_attr(not(feature = "container_unseal"), allow(dead_code))]
+#[inline]
+unsafe fn alloc_maydangle(layout: Layout) -> NonNull<()> {
+    if layout.size() == 0 {
+        NonNull::dangling()
+    } else {
+        unsafe { nonnull_alloc(layout) }.cast()
+    }
+}
+
+#[cfg_attr(not(feature = "container_unseal"), allow(dead_code))]
+#[inline]
+unsafe fn nonnull_dealloc(ptr: NonNull<u8>, layout: Layout) {
+    unsafe { dealloc(ptr.as_ptr(), layout) }
+}
+
+#[cfg_attr(not(feature = "container_unseal"), allow(dead_code))]
+#[inline]
+unsafe fn dealloc_maydangle(ptr: NonNull<u8>, layout: Layout) {
+    if layout.size() == 0 {
+        return;
+    }
+    unsafe { nonnull_dealloc(ptr.cast(), layout) }
+}
