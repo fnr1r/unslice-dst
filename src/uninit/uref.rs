cbe024eacaaeedb2ed94a63d3b213fc59b0e55a2
fnrir <fnr1r0@protonmail.com>
2026-02-04 19:58:32 +0100

    feat(uninit): add into_canonical for Uninit types

--- src/uninit/uref.rs
+++ src/uninit/uref.rs
@@ -1,4 +1,11 @@
-use core::{marker::PhantomData, mem::transmute, ops::Deref, ptr::NonNull};
+use core::{
+    marker::PhantomData,
+    mem::{MaybeUninit, transmute},
+    ops::Deref,
+    ptr::NonNull,
+};
+
+use crate::cast::dst_cast_nonnull;
 
 /// [NonNull] but valid for a set lifetime.
 ///
@@ -13,5 +20,27 @@ impl<'a, T: ?Sized> Deref for UninitRef<'a, T> {
     #[inline]
     fn deref(&self) -> &Self::Target {
         unsafe { transmute(self) }
+    }
+}
+
+impl<'a, T> UninitRef<'a, T> {
+    /// Convert self into the canonical Rust representation
+    ///
+    /// Possible here since T is [`Sized`].
+    #[inline]
+    pub const fn into_canonical(self) -> &'a MaybeUninit<T> {
+        // SAFETY: The pointer is valid
+        unsafe { self.0.cast().as_ref() }
+    }
+}
+
+impl<'a, T> UninitRef<'a, [T]> {
+    /// Convert self into the canonical Rust representation
+    ///
+    /// Possible here since T is a [`slice`](primitive@slice).
+    #[inline]
+    pub const fn into_canonical(self) -> &'a [MaybeUninit<T>] {
+        // SAFETY: The pointer is valid
+        unsafe { dst_cast_nonnull(self.0).as_ref() }
     }
 }
