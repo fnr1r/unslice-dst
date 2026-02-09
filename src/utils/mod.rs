56f7abf540ccb698bf7d25ac1b65e94f7831f464
fnrir <fnr1r0@protonmail.com>
2026-02-09 19:25:24 +0100

    feat(alloc): don't use mutex for lcg rand
    this is test data. we don't need shared state here.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,5 +4,5 @@ pub(crate) mod nalloc;
 pub(crate) mod slice;
 mod transmute_lax;
 
-#[cfg(all(test, feature = "std"))]
+#[cfg(test)]
 pub(crate) mod rand;
--- src/utils/rand.rs
+++ src/utils/rand.rs
@@ -3,13 +3,13 @@
 //! Changes from musl:
 //!
 //! - Idiomatic-ish Rust API
-//! - The [`next`] explicitly uses wrapping maths.
-//! - Uses a [`Mutex`] for thread-safety (since tests may be multi-threaded)
+//! - The [`next`] function explicitly uses wrapping maths.
+//! - The RNG is instanced. No statics or shared state.
 //! - The seed is set to `1337_u64` by default
 //!
+//! TODO: Maybe use a different seed for different tests?
+//!
 //! [`next`]: musl_lcg_next
-
-use std::sync::{Mutex, MutexGuard};
 
 const MUSL_MULTIPLIER: u64 = 6364136223846793005;
 
@@ -17,29 +17,18 @@ const fn musl_lcg_next(value: u64) -> u64 {
     value.wrapping_mul(MUSL_MULTIPLIER).wrapping_add(1)
 }
 
-static MUSL_SEED: Mutex<u64> = Mutex::new(1337);
-
-/// Lock and ignore poison error
-fn mutex_ilock<T>(lock: &Mutex<T>) -> MutexGuard<'_, T> {
-    match lock.lock() {
-        Ok(res) => res,
-        Err(error) => error.into_inner(),
-    }
-}
+const MUSL_SEED: u64 = 1337;
 
 #[derive(Debug)]
-pub(crate) struct MuslRand(MutexGuard<'static, u64>);
+pub(crate) struct MuslRand(u64);
 
 impl MuslRand {
-    pub(crate) fn lock() -> Self {
-        Self(mutex_ilock(&MUSL_SEED))
-    }
     pub(crate) fn next_inf(&mut self) -> i32 {
-        *self.0 = musl_lcg_next(*self.0);
-        (*self.0 >> 33) as _
+        self.0 = musl_lcg_next(self.0);
+        (self.0 >> 33) as _
     }
 }
 
 pub(crate) fn musl_rand_with_iter<T>(f: impl FnOnce(&mut MuslRand) -> T) -> T {
-    f(&mut MuslRand::lock())
+    f(&mut MuslRand(MUSL_SEED))
 }
