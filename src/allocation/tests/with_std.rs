ec59ad801e6775c7d3994da65a8b59bbd05aa296
fnrir <fnr1r0@protonmail.com>
2026-02-09 17:54:16 +0100

    feat(alloc): add tests for (A)Rc pointers

--- src/allocation/tests/with_std.rs
+++ src/allocation/tests/with_std.rs
@@ -1,5 +1,5 @@
-use alloc::boxed::Box;
-use core::ptr::NonNull;
+use alloc::{boxed::Box, rc::Rc, sync::Arc};
+use core::{ops::Deref, ptr::NonNull};
 
 use crate::{allocation::AllocSliceDst, utils::rand::musl_rand_with_iter};
 
@@ -9,12 +9,26 @@ fn acq_init_data() -> Box<[f32; 42]> {
     res
 }
 
-#[test]
-fn assert_works_for_slice() {
+fn do_check_works<T: AllocSliceDst<Target = [f32]> + Deref<Target = [f32]>>() {
     let initial_data = acq_init_data();
     let init = |mut ptr: NonNull<[f32]>| {
         unsafe { ptr.as_mut() }.copy_from_slice(initial_data.as_ref());
     };
-    let data = unsafe { <Box<[f32]>>::new_slice_dst(42, init) };
+    let data = unsafe { T::new_slice_dst(42, init) };
     assert_eq!(*initial_data, *data);
+}
+
+#[test]
+fn assert_works_for_box_slice() {
+    do_check_works::<Box<_>>();
+}
+
+#[test]
+fn assert_works_for_rc_slice() {
+    do_check_works::<Rc<_>>();
+}
+
+#[test]
+fn assert_works_for_arc_slice() {
+    do_check_works::<Arc<_>>();
 }
